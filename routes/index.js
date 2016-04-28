36bb29920563697b87452a40bc9710ff98d9d9a7
Andrew Wadman & James Sangalli <pair+andrew-travis-wadman.james-sangalli@devbootcamp.com>
2016-04-28 16:21:10 +1200

    Add user data API page

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var fs = require("fs")
 
-<<<<<<< HEAD
 var knex = require('knex')({
   client: 'sqlite3',
   connection: {
@@ -14,7 +13,7 @@ var knex = require('knex')({
 //transfer balance
 //transaction history
 //login
-
+// API SET UP
 router.get("/api/getBalance/:id", function(req, res) {
   var id = req.params.id;
 
@@ -23,7 +22,19 @@ router.get("/api/getBalance/:id", function(req, res) {
       console.log(resp);
       res.send(resp);
     })
-=======
+})
+
+router.get("/api/users/:id", function(req, res) {
+  var id = req.params.id;
+
+  knex.select('first_name','last_name', 'balance').from('users').innerJoin('balances', 'users.id','balances.id').where('balances.id',id)
+    .then(function(resp) {
+      console.log(resp);
+      res.send(resp);
+    })
+})
+
+// Client side rendering
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('entry', { title: 'Express' });
@@ -31,7 +42,6 @@ router.get('/', function(req, res, next) {
 
 router.get('/client', function(req, res, next) {
   res.render('client', { title: 'Express' });
->>>>>>> 4c867b91624fe81b3bea697f8a230e7fef13f423
 });
 
 module.exports = router;
