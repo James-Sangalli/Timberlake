fe3326a9e74110e4459bfc3642ea63f09dfd466b
Andrew Wadman & James Sangalli <pair+andrew-travis-wadman.james-sangalli@devbootcamp.com>
2016-04-28 15:27:54 +1200

    Add sql for balance check"

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,19 @@ var express = require('express');
 var router = express.Router();
 var fs = require("fs")
 
+var knex = require('knex')({
+  client: 'sqlite3',
+  connection: {
+    filename: './database/dev.sqlite3'
+  },
+  useNullAsDefault: true
+})
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
 //getbalance
 //transfer balance
@@ -14,15 +23,27 @@ router.get('/', function(req, res, next) {
 
 router.get("/api/getBalance/:id", function(req, res) {
   var id = req.params.id - 1;
-  fs.readFile("./userData.json", "utf8", (err, data) => {
-    if (err) throw err;
-    var results = JSON.parse(data)
-    var userInfo = results.filter(function(i) {
-      return i.id === id
-    });
-    res.send(JSON.stringify(userInfo[0].balance))
-  })
+  // fs.readFile("./userData.json", "utf8", (err, data) => {
+  //   if (err) throw err;
+  //   var results = JSON.parse(data)
+  //   var userInfo = results.filter(function(i) {
+  //     return i.id === id
+  //   });
+  //   res.send(JSON.stringify(userInfo[0].balance))
+  // })
 
+  // knex.select().from('users').where('id',id)
+  // .then(function(resp){
+  //   console.log(resp)
+  //   res.send(resp[0].last_name)
+  // })
+
+knex.select('balance').from('users').innerJoin('transactions', 'users.id', 'transactions.id').where('transactions.id',id)
+  .then(function(resp) {
+
+    console.log(resp);
+    res.send(resp);
+  })
 });
 
 module.exports = router;
