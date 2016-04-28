3e8f7a3e3f2330de300ba859f0ba9f8aa800faf4
Andrew Wadman & James Sangalli <pair+andrew-travis-wadman.james-sangalli@devbootcamp.com>
2016-04-28 15:30:51 +1200

    refactored

--- routes/index.js
+++ routes/index.js
@@ -10,40 +10,20 @@ var knex = require('knex')({
   useNullAsDefault: true
 })
 
-
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
 //getbalance
 //transfer balance
 //transaction history
 
 
 router.get("/api/getBalance/:id", function(req, res) {
-  var id = req.params.id - 1;
-  // fs.readFile("./userData.json", "utf8", (err, data) => {
-  //   if (err) throw err;
-  //   var results = JSON.parse(data)
-  //   var userInfo = results.filter(function(i) {
-  //     return i.id === id
-  //   });
-  //   res.send(JSON.stringify(userInfo[0].balance))
-  // })
+  var id = req.params.id;
 
-  // knex.select().from('users').where('id',id)
-  // .then(function(resp){
-  //   console.log(resp)
-  //   res.send(resp[0].last_name)
-  // })
+  knex.select('balance').from('users').innerJoin('transactions', 'users.id', 'transactions.id').where('transactions.id',id)
+    .then(function(resp) {
 
-knex.select('balance').from('users').innerJoin('transactions', 'users.id', 'transactions.id').where('transactions.id',id)
-  .then(function(resp) {
-
-    console.log(resp);
-    res.send(resp);
-  })
+      console.log(resp);
+      res.send(resp);
+    })
 });
 
 module.exports = router;
