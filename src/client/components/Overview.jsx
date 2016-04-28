ec1a497439a8400823905d6611114a8aec3f2a2f
tony-luisi <t.luisi@gmail.com>
2016-04-28 16:04:29 +1200

    added components

--- src/client/components/Overview.jsx
+++ src/client/components/Overview.jsx
@@ -1,14 +1,23 @@
 import React, {Component} from 'react'
+import request from 'superagent'
 
 export default class Overview extends Component {
   constructor(props){
     super(props)
   }
 
+  componentDidMount(){
+
+  }
+
   render(){
     return(
       <div>
-        <h2>Hi there im Overview</h2>
+        <h2>Hello xxx</h2>
+        <h3>Your balance is xxx</h3>
+        <h4>Would you like to:</h4>
+        <p>Make a payment</p>
+        <p>View transactions</p>
       </div>
     )
   }
